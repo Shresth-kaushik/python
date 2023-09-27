ab1ee801ff73f909e77501f52aea76ad1ce10acf
Shresth Kaushik <kaushikshresth22@gmail.com>
2023-09-27 18:15:50 +0530

    first commit

---
+++ Day_2/Day_2.py
@@ -0,0 +1,26 @@
+print(len("Hello"))
+
+# Data types in the python
+
+# 1 . String
+# Subscripting : Pulling out the character from the string 
+print("Hello"[0])
+
+# Concontation of the string 
+print("123" + "456")
+
+# Boolean
+# Float 
+# String
+# int 
+
+
+# type() => to check the data type of the function 
+
+num_char = len(input("Enter your name ? "));
+
+# print("Name is having " + num_char + " characters "); 
+# can not concatenate the string + intger + string
+# string is only concatenated with the string 
+
+print(type(num_char))
---
+++ intro.py
@@ -0,0 +1,5 @@
+print("Hell World") 
+# How to take input in python 
+# input("Enter your name")
+# print("Enter the name ")
+print("Hello " + input("What is your name ? "))
