0b75d7cb89740b725f6bd256b24d935335dc2449
Shresth Kaushik <kaushikshresth22@gmail.com>
2024-01-07 08:39:56 +0530

    7_jan

---
+++ List/list.py
@@ -0,0 +1,12 @@
+# List is Data structure to store the hetrogeneous data .
+# List is just like array but differ in the data present in the array or list 
+
+l = [1,"Kaushik" , 9.34 , True , 10] 
+print(l)
+print(l[2])
+
+# List silicing
+list  = [1,4,3,15,17,12]
+print(list[4:])
+print(list[-4:])
+print(list[1:4])
